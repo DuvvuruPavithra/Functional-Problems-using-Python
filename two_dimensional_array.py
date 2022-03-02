07a41ea2e577eff62b1936b7944ceefc14eb0d94
DuvvuruPavithra <duvvurupavithra28@gmail.com>
2022-03-02 21:58:58 +0530

    Added: print the function to print the two dimensional array

---
+++ two_dimensional_array.py
@@ -0,0 +1,22 @@
+def array_input(rows, cols):
+    array = []
+    for i in range(rows):
+        columns = []
+        for j in range(cols):
+            values = int(input("enter the values:"))
+            columns.append(values)
+        array.append(columns)
+
+    # For printing the matrix
+    for i in range(rows):
+        for j in range(cols):
+            print(array[i][j], end=" ")
+        print()
+
+
+if __name__ == '__main__':
+    rows = int(input("enter the row size"))
+    cols = int(input("enter the columns size"))
+array_input(rows, cols)
+
+
